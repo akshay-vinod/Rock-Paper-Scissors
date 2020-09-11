f4507bce218fbca7ac139cde54c91b6a38ed3dea
akshay-vinod <akshayvinod468@gmail.com>
2020-09-12 00:31:32 +0530

    new game is advanced

--- app.js
+++ app.js
@@ -23,13 +23,12 @@ const game = () => {
             option.addEventListener('click', function () {
                 //random computer choice
                 const computerNumber = Math.floor(Math.random() * 3);
-                const computerChoice = computerOptions[computerNumber];
+                let computerChoice = computerOptions[computerNumber];
                 //to find who wins
-                winner(option.textContent, computerChoice);
+                let flag = winner(option.textContent, computerChoice);
                 //update image
                 playerHand.src = `./assets/${option.textContent}.png`;
                 computerHand.src = `./assets/${computerChoice}.png`;
-
             })
         })
 
@@ -51,7 +50,6 @@ const game = () => {
                 if (pScore === 10) {
                     reset();
                 }
-
             }
             else {
                 winnerUpdate.textContent = "Computer Scored";
@@ -60,6 +58,7 @@ const game = () => {
                 if (cScore === 10) {
                     reset();
                 }
+
 
             }
         }
@@ -73,7 +72,6 @@ const game = () => {
                 if (pScore === 10) {
                     reset();
                 }
-
             }
             else {
                 winnerUpdate.textContent = "Computer Scored";
@@ -81,9 +79,7 @@ const game = () => {
                 document.querySelector(".computer p").textContent = cScore;
                 if (cScore === 10) {
                     reset();
-
                 }
-
             }
         }
         else if (playerChoice === "scissors") {
@@ -106,32 +102,39 @@ const game = () => {
                 if (cScore === 10) {
                     reset();
                 }
-
             }
         }
 
     }
     const reset = () => {
+        const winnerUpdate = document.querySelector(".winner");
+        const modal = document.querySelector(".modal");
+        const newgamebtn = document.querySelector(".modal-body button");
+        const span = document.getElementsByClassName("close")[0];
+        const reset_value = "rock";
         if (pScore === 10) {
-            const winnerUpdate = document.querySelector(".winner");
-            const modal = document.querySelector(".modal");
-            const newgamebtn = document.querySelector(".modal-body button");
-            const span = document.getElementsByClassName("close")[0];
             winnerUpdate.textContent = "Player wins";
+            document.querySelector(".newgame").classList.remove("fadeOut");
             //disable btn clicks
-            document.querySelector(".options").classList.add("avoid-clicks");
+            document.querySelector(".options").classList.add("fadeOut");
             //update modal Text
             document.querySelector(".modal h4").textContent = "You Won The Game"
-            // When the user clicks the button, open the modal
+            // open the modal
             modal.style.display = "block";
             // When the user clicks on <span> (x), close the modal
             span.addEventListener("click", () => {
                 modal.style.display = "none";
+                //reset image to rock
+                playerHand.src = `./assets/${reset_value}.png`;
+                computerHand.src = `./assets/${reset_value}.png`;
             });
             // When the user clicks anywhere outside of the modal, close it
             window.addEventListener("click", (event) => {
                 if (event.target === modal) {
                     modal.style.display = "none";
+                    //reset image to rock
+                    playerHand.src = `./assets/${reset_value}.png`;
+                    computerHand.src = `./assets/${reset_value}.png`;
                 }
             });
             //when uer click new game
@@ -142,33 +145,37 @@ const game = () => {
 
         }
         else {
-            const winnerUpdate = document.querySelector(".winner");
-            const modal = document.querySelector(".modal");
-            const newgamebtn = document.querySelector(".modal-body button");
-            const span = document.getElementsByClassName("close")[0];
             winnerUpdate.textContent = "Computer Wins";
+            //new game button
+            document.querySelector(".newgame").classList.remove("fadeOut");
             //disable btn clicks
-            document.querySelector(".options").classList.add("avoid-clicks");
-            // When the user clicks the button, open the modal 
+            document.querySelector(".options").classList.add("fadeOut");
+            // open the modal 
             modal.style.display = "block";
             // When the user clicks on <span> (x), close the modal
             span.addEventListener("click", () => {
                 modal.style.display = "none";
+                /* //reset image to rock
+                   playerHand.src = `./assets/${reset_value}.png`;
+                   computerHand.src = `./assets/${reset_value}.png`;*/
             });
             // When the user clicks anywhere outside of the modal, close it
             window.addEventListener("click", (event) => {
                 if (event.target == modal) {
                     modal.style.display = "none";
+                    /* //reset image to rock
+                     playerHand.src = `./assets/${reset_value}.png`;
+                     computerHand.src = `./assets/${reset_value}.png`;*/
                 }
             });
-            //when uer click new game
-            newgamebtn.addEventListener("click", () => {
-                location.reload();
-            });
         }
-    }
-    const newGame = () => {
-
+        //when uer click new game
+        document.querySelector(".newgame button").addEventListener("click", () => {
+            location.reload();
+        });
+        newgamebtn.addEventListener("click", () => {
+            location.reload();
+        });
     }
     showMatch();
     playmatch();
